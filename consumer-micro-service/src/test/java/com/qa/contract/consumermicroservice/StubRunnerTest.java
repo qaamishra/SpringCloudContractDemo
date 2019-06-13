38b35c4f069e50df8f81c4d7ebce188ba1358231
qa.amishra@gmail.com <qa.amishra@gmail.com>
2019-06-13 19:30:24 +0530

    Completed demo

--- consumer-micro-service/src/test/java/com/qa/contract/consumermicroservice/StubRunnerTest.java
+++ consumer-micro-service/src/test/java/com/qa/contract/consumermicroservice/StubRunnerTest.java
@@ -12,17 +12,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ConsumerMicroServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL,
-        ids = "com.qa.contract:provider-micro-service:+:stubs:8091")
-//groupId:artifactId:version(+ means latest version):stubs:8091
+        ids = "com.qa.contract:provider-micro-service:+:stubs:8090")
+//groupId:artifactId:version(+ means latest version):stubs:8090 Stub port number should be same as mentioned in provider
 
 /*
 Provider artifact , where stub is present
