78796a1a440ca44b06444512d38e14bd77ce6a56
qa.amishra@gmail.com <qa.amishra@gmail.com>
2019-06-24 12:04:12 +0530

    Added Rule annotation

--- consumer-micro-service/src/test/java/com/qa/contract/consumermicroservice/StubRunnerTest.java
+++ consumer-micro-service/src/test/java/com/qa/contract/consumermicroservice/StubRunnerTest.java
@@ -1,13 +1,18 @@
 package com.qa.contract.consumermicroservice;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.StubFinder;
+import org.springframework.cloud.contract.stubrunner.junit.StubRunnerRule;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,8 +39,23 @@ public class StubRunnerTest {
     @Autowired
     private MockMvc mockMvc;
 
+  /*  @StubRunnerPort("provider-micro-service")
+    int port;
+
+    @Rule
+    public StubRunnerRule rule = new StubRunnerRule()
+            .downloadStub("com.qa.contract", "provider-micro-service")
+            .withPort(port);
+
+    @Autowired
+    StubFinder stubFinder;*/
+
     @Test
     public void given_WhenPassEvenNumberInQueryParam_ThenReturnEven() throws Exception {
+       /* System.out.println("==========port==========="+port+"==========port===========");
+        int port = stubFinder.findStubUrl("provider-micro-service").getPort();
+        System.out.println("======stubFinder======="+port+"===========stubFinder====");*/
+
         mockMvc.perform(MockMvcRequestBuilders.get("/check?number=2")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
